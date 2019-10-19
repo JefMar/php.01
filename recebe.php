38471659d982976a724b0e7c2d2717c56e7d78a1
Jeff <jefff>
2019-10-18 21:45:36 -0300

    a

--- recebe.php
+++ recebe.php
@@ -1,10 +1,13 @@
 <?php
 
-    echo "Olá, Mundo!";
+    $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
+    $email = $_POST["email"];
+    
 
-    $nome = "Nelson";
-
-    echo $nome;
+    echo "Nome: ".$nome."<br>";
+    echo "Idade: ".$idade."<br>";
+    echo "E-mail: ".$email."<br>";
 
 
 ?>
