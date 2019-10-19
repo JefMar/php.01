4f669e10113732638b09503e3939343e24c00158
Jeff <jefff>
2019-10-18 21:50:09 -0300

    a

--- recebe.php
+++ recebe.php
@@ -4,7 +4,7 @@
     $idade = $_POST["idade"];
     $email = $_POST["email"];
     
-
+    if()
     echo "Nome: ".$nome."<br>";
     echo "Idade: ".$idade."<br>";
     echo "E-mail: ".$email."<br>";
